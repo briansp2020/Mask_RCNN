2a6b1acffb16115ef642efb19bfb6e9586107df0
Brian S. Park <brianspark@hotmail.com>
2018-02-04 09:37:10 -0800

    Added more data augmentation to the model and cyclical learning rate.

--- model.py
+++ model.py
@@ -1294,6 +1294,7 @@ def load_image_gt(dataset, config, image_id, augment=False, augmentation=None,
     _idx = np.sum(mask, axis=(0, 1)) > 0
     mask = mask[:, :, _idx]
     class_ids = class_ids[_idx]
+
     # Bounding boxes. Note that some boxes might be all zeros
     # if the corresponding mask got cropped out.
     # bbox: [num_instances, (y1, x1, y2, x2)]
